d6410f6fb286034610813b9ffdde1a9751686d1a
Bruno Vargas <brunosoaresvargas@gmail.com>
2024-11-02 09:42:46 -0300

    adicionei comentários explicativos

--- remove_background.py
+++ remove_background.py
@@ -2,12 +2,13 @@
 from rembg import remove
 # fornece ao interpretador Python recursos de edição de imagem
 from PIL import Image
-
+# Aqui editamos o nome da imagem de entrada.
 img_entrada = 'original.jpeg'
+# Aqui definimos o nome da imagem de saída.
 img_saida = 'editado.png'
+
 input = Image.open(img_entrada)
 output = remove(input)
 
 #salvando a imagem de saída
-
 output.save(img_saida, 'PNG')
